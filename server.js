6a9cb1b9df0cb33be989309901c40799a6a2e59d
Glen Quirós Ortiz <glenquor@gmail.com>
2022-01-28 10:37:31 -0600

    server is working perfectly

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
-
-
-
+app.use('/', require('./routes/routes'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
